cc595f8cca02085e9f2d2a794e1238d3006694d0
Vadim <91338647+pticagamaun@users.noreply.github.com>
2022-12-11 19:23:33 +0300

    Initial commit

---
+++ fitApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  fitApp
+//
+//  Created by Vadim on 11.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
